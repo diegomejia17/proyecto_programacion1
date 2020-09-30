d167099723b32cb01d092b4b639f48f8d5fa5794
Javier Ayala <franciscojavier20002017@gmail.com>
2020-09-29 23:17:42 -0600

    agregando los switch y funciones

--- proyecto1.cpp
+++ proyecto1.cpp
@@ -1,32 +1,71 @@
 #include<iostream>
 #include<string.h>
 #include<ctype.h>
-<<<<<<< HEAD
-void ingresar();
-=======
- 
+
 using namespace std;
 void registro_pago();
->>>>>>> c21f5b1d074afee41cc4fb90b1ced6f2e76c4bad
+void Registros();
+
+
 int main()
     {
+        int opccion, Opc_Registro;
         cout<<"CONTROL DE RECIBO DE AGUA"<<endl;
-        switch()
+        cout<<"1: Registro de usuario"<<endl;
+        cout<<"2: Registro de consumo de agua mensual"<<endl;
+        cout<<"3: Registro de pago de recibo de agua"<<endl;
+        cout<<"4: Informe de pago de usuarios"<<endl;
+        cout<<"5: salir"<<endl;
+        cin>>opccion;
+
+        do{
+            switch(opccion)
+                case 1: {
+                            Registros();
+                        }break;
+                case 2: {
+
+                        }break;
+                case 3: {
+
+                        }break;
+                case 4: {
+
+                        }break;
+                default{
+
+                        }break
+            }while(opccion > 0);
             
 
 
         getchar();
         cin.get();
     }
-<<<<<<< HEAD
-ingresar()
-    {
 
+void Registros()
+    {
+        cout<<"Registros"<<endl;
+        cout<<"1: Nueva cuenta"<<endl;
+        cout<<"2: Editar cuenta"<<endl;
+        cout<<"3: Eliminar cuenta"<<endl;
+        cin>>Opc_Registro;
+            do{
+                switch(opccion)
+                    case 1: {
+                           }break;
+                    case 2: {
+                            }break;
+                    case 3: {
+                            }break;
+                    default {
+                            }break;
+
+                    }while(Opc_Registro > 0);
     }
-=======
+
 
 void registro_pago()
 {
 	cout << "REGISTRO DE PAGO" << endl;
 }
->>>>>>> c21f5b1d074afee41cc4fb90b1ced6f2e76c4bad
