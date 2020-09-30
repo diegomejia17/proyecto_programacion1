35da7aed4fe9bfd9830de047490107edca8bce07
Javier Ayala <franciscojavier20002017@gmail.com>
2020-09-29 22:52:29 -0600

    switch

--- proyecto1.cpp
+++ proyecto1.cpp
@@ -1,10 +1,18 @@
 #include<iostream>
 #include<string.h>
 #include<ctype.h>
-
+void ingresar();
 int main()
     {
+        cout<<"CONTROL DE RECIBO DE AGUA"<<endl;
+        switch()
+            
+
 
         getchar();
         cin.get();
+    }
+ingresar()
+    {
+
     }
