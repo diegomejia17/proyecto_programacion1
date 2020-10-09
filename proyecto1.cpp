afbd12d0bffce925934c64cfc1c19d3636fca1bf
Javier Ayala <franciscojavier20002017@gmail.com>
2020-10-08 23:32:31 -0600

    modificacion

--- proyecto1.cpp
+++ proyecto1.cpp
@@ -1,15 +1,21 @@
 #include<iostream>
+#include<stdlib.h>
+#include<conio.h>
 #include<string.h>
-#include<ctype.h>
+#include<windows.h>
 
 using namespace std;
-void registro_pago();
 
-void Registros();
+const int N = 100;
+
+void Registros(char Nombres[N][100], char Apellidos[N][100],int Dui[N]);
+
 int main()
     {
-
-        int opccion, Opc_Registro;
+        char Nombres[N][100] = {"\0"}, Apellidos[N][100] = {"\0"};
+        int Dui[N];
+        int opccion;
+        do{
         cout<<"CONTROL DE RECIBO DE AGUA"<<endl;
         cout<<"1: Registro de usuario"<<endl;
         cout<<"2: Registro de consumo de agua mensual"<<endl;
@@ -17,24 +23,35 @@ int main()
         cout<<"4: Informe de pago de usuarios"<<endl;
         cout<<"5: salir"<<endl;
         cin>>opccion;
+        system("Cls");
 
-        do{
+        
             switch(opccion)
-                case 1: {
-                            Registros();
-                        }break;
-                case 2: {
+                {
 
-                        }break;
-                case 3: {
+                        case 1: 
+                                {
+                                   Registros(Nombres, Apellidos, Dui);
+                                   system("Cls");
+                                } break;
+                        case 2: 
+                                {
 
-                        }break;
-                case 4: {
+                                }break;
+                        case 3:
+                                {
 
-                        }break;
-                default{
+                                }break;
+                        case 4:
+                                {
 
-                        }break
+                                }break;
+                        default:
+                                {
+
+                                }break;
+
+                }        
             }while(opccion > 0);
             
 
@@ -42,30 +59,83 @@ int main()
         getchar();
         cin.get();
     }
+void gotoxy(int x, int y)
+    {//esta funcion permite el posicionamiento del curso x, y
+        HANDLE hcon;
+        hcon=GetStdHandle(STD_OUTPUT_HANDLE);
+        COORD dwPos;
+        dwPos.X=x;
+        dwPos.Y=y;
+        SetConsoleCursorPosition(hcon,dwPos);
+    }
 
+void Registros(char Nombres[N][100], char Apellidos[N][100], int Dui[N])
 
-ingresar()
-
-void Registros()
-
-    {
-        cout<<"Registros"<<endl;
-        cout<<"1: Nueva cuenta"<<endl;
-        cout<<"2: Editar cuenta"<<endl;
-        cout<<"3: Eliminar cuenta"<<endl;
-        cin>>Opc_Registro;
+    { 
+            int i=0, Opc_Registro;
             do{
-                switch(opccion)
-                    case 1: {
-                           }break;
-                    case 2: {
-                            }break;
-                    case 3: {
-                            }break;
-                    default {
-                            }break;
+                cout<<"Registros"<<endl;
+                cout<<"1: Nueva cuenta"<<endl;
+                cout<<"2: Editar cuenta"<<endl;
+                cout<<"3: Eliminar cuenta"<<endl;
+                cout<<"4: Menu principal"<<endl;
+                cin>>Opc_Registro;
+                system("Cls");
+                switch(Opc_Registro)
+                        {
+                                case 1: 
+                                        {
+                                                fflush(stdin);
+                                                cout<<"Ingresa los nombre"<<endl;
+                                                gets(Nombres[i]);
+                                                cout<<"Ingresa Tus apellidos"<<endl;
+                                                gets(Apellidos[i]);
+                                                cout<<"Ingresa el numero de DUI"<<endl;
+                                                cin>>Dui[i];
+                                                system("Cls");
+                                                i++;
+                                        }break;
+                                case 2: 
+                                        {
+                                                gotoxy(0,0);
+                                                cout<<"N de usuario";
+                                                gotoxy(25,0);
+                                                cout<<"Numero de cuenta";
+                                                gotoxy(50,0);
+                                                cout<<"Nombre";
+                                                gotoxy(75,0);
+                                                cout<<"Apellido";
+                                                gotoxy(100,0);
+                                                cout<<"Dui"<<endl;
+                                                cout<<"\n";
+                                                for(int j = 0; j <= i; j++)
+                                                        {
+                                                                gotoxy(0,j+1);
+                                                                cout<<j;
+                                                                gotoxy(25,j+1);
+                                                                cout<<"Numero co";
+                                                                gotoxy(50,j+1);
+                                                                cout<<Nombres[j];
+                                                                gotoxy(75,j+1);
+                                                                cout<<Apellidos[j];
+                                                                gotoxy(100,j+1);
+                                                                cout<<Dui[j];
+                                                                
+                                                        }
+                                                        system("pause>null");
+                                                        system("Cls");
+                                        }break;
+                                case 3:
+                                        {
 
-                    }while(Opc_Registro > 0);
+                                        }break;
+                                default :
+                                        {
+
+                                        }break;
+                        }
+
+                    }while(Opc_Registro != 4);
     }
 
 
