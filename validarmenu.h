310e6b3a2bfe72df15b5168de01b6bbca38dbb79
Javier Ayala <franciscojavier20002017@gmail.com>
2020-11-05 20:47:19 -0600

    nuevo

--- validarmenu.h
+++ validarmenu.h
@@ -6,12 +6,9 @@ using namespace std;
 int Validarmenu(char Nu[2])//a la funcion se le debe pasar el array que contiene el numero, y el numero de posiciones que se debe evaluar
         {
                 char c1 ;
-                for (int  i1 = 0; i1 < 1; i1++)
-                {
-                        c1=Nu[i1];
+                        c1=Nu[0];
                         if(isdigit (c1) == 0)//Falso si el caracter no es letra
                                 {
                                         return 0;
                                 }    
-                }
         }
