89ca37eb92c9968650283e7693dbc62730c792a7
Gerson <gersonstanleyescobarserrano@gmail.com>
2021-01-18 23:32:57 -0600

    Case 4 75% listo

--- funciones_Christian/costo.cpp
+++ funciones_Christian/costo.cpp
@@ -1,78 +1,126 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int costo(float consumo_mensual)
+{
+
+    float tax = consumo_mensual; //consumo de agua mensual
+    float taxAl = 0;             //tarifa de acantarillado
+    float taxAC = 0;             //tarifa de acuaducto
+    float total = 0;             //total a pagar
+
+    if (tax >= 0.00 && tax <= 10.00)
     {
-
-       float tax = consumo_mensual;//consumo de agua mensual
-       float taxAl = 0;//tarifa de acantarillado 
-       float taxAC = 0;//tarifa de acuaducto
-       float total = 0;//total a pagar
-
-            if(tax>=0.00 && tax<=10.00){
-        taxAl=0.10;
-        taxAC=2.29;
-    }else if(tax>10.00 && tax<=20.3){
-        taxAl=5.0+0.10;
-        taxAC=0.21+2.29;
-    }else if(tax>20.30 && tax<=30.00){
-        taxAl=1.80+0.10;
-        if(tax>20 && tax<22){
-            taxAC=2.29+0.25;
-        }else if(tax>=22.00 && tax<23){
-            taxAC=2.29+0.28;
-        }else if(tax>=23.00 && tax>24.00){
-            taxAC=0.31+2.29;
-        }else if(tax>=24 && tax<25){
-            taxAC=2.29+0.34;
-        }else{
-            taxAC=2.29+0.37;
-        }
-    }else if(tax>30 && tax<=40){
-        taxAl=0.10+3.00;
-        if(tax>30 && tax<=31){
-            taxAC=2.29+0.42;
-        }else if(tax>31 && tax<=32){
-            taxAC=2.29+0.48;
-        }else if(tax>32 && tax<=33){
-            taxAC=2.29+0.54;
-        }else if(tax>33 && tax<=34){
-            taxAC=2.29+0.64;
-        }else if(tax>34 && tax<=40){
-            taxAC=2.29+0.76;
-        }
-    }else if(tax>40 && tax<=50){
-        taxAl=0.10+4.00;
-        if(tax<40 && tax<=41){
-            taxAC=2.29+0.90;
-        }else if(tax<41 && tax<=42){
-            taxAC=2.29+1.05;
-        }else if(tax>42 && tax<=43){
-            taxAC=2.29+1.20;
-        }else if(tax>43 && tax<=44){
-            taxAC=2.29+40;
-        }else if(tax>44 && tax<=50){
-            taxAC=2.29+1.65;
-        }
-    }else if(tax>50 && tax<=100){
-        taxAl=7.50;
-        if(tax>50 && tax>=60){
-            taxAC=2.29+1.90;
-        }else if(tax>60 && tax<=70){
-            taxAC=2.29+2.20;
-        }else if(tax>70 && tax<=90){
-            taxAC=2.29+2.50;
-        }else if(tax>90 && tax<=100){
-            taxAC=2.29+2.90;
-        }
-    }else if(tax>100 && tax<=500){
-        taxAl=0.10+10.00;
-        taxAC=2.29+3.40;
-    }else if(tax>500){
-        taxAl=0.10+20.00;
-        taxAC=2.29+3.90;
+        taxAl = 0.10;
+        taxAC = 2.29;
     }
-   return total=(tax*taxAC)+taxAl;
-   system("pause>null");
-   
+    else if (tax > 10.00 && tax <= 20.3)
+    {
+        taxAl = 5.0 + 0.10;
+        taxAC = 0.21 + 2.29;
     }
+    else if (tax > 20.30 && tax <= 30.00)
+    {
+        taxAl = 1.80 + 0.10;
+        if (tax > 20 && tax < 22)
+        {
+            taxAC = 2.29 + 0.25;
+        }
+        else if (tax >= 22.00 && tax < 23)
+        {
+            taxAC = 2.29 + 0.28;
+        }
+        else if (tax >= 23.00 && tax > 24.00)
+        {
+            taxAC = 0.31 + 2.29;
+        }
+        else if (tax >= 24 && tax < 25)
+        {
+            taxAC = 2.29 + 0.34;
+        }
+        else
+        {
+            taxAC = 2.29 + 0.37;
+        }
+    }
+    else if (tax > 30 && tax <= 40)
+    {
+        taxAl = 0.10 + 3.00;
+        if (tax > 30 && tax <= 31)
+        {
+            taxAC = 2.29 + 0.42;
+        }
+        else if (tax > 31 && tax <= 32)
+        {
+            taxAC = 2.29 + 0.48;
+        }
+        else if (tax > 32 && tax <= 33)
+        {
+            taxAC = 2.29 + 0.54;
+        }
+        else if (tax > 33 && tax <= 34)
+        {
+            taxAC = 2.29 + 0.64;
+        }
+        else if (tax > 34 && tax <= 40)
+        {
+            taxAC = 2.29 + 0.76;
+        }
+    }
+    else if (tax > 40 && tax <= 50)
+    {
+        taxAl = 0.10 + 4.00;
+        if (tax < 40 && tax <= 41)
+        {
+            taxAC = 2.29 + 0.90;
+        }
+        else if (tax < 41 && tax <= 42)
+        {
+            taxAC = 2.29 + 1.05;
+        }
+        else if (tax > 42 && tax <= 43)
+        {
+            taxAC = 2.29 + 1.20;
+        }
+        else if (tax > 43 && tax <= 44)
+        {
+            taxAC = 2.29 + 40;
+        }
+        else if (tax > 44 && tax <= 50)
+        {
+            taxAC = 2.29 + 1.65;
+        }
+    }
+    else if (tax > 50 && tax <= 100)
+    {
+        taxAl = 7.50;
+        if (tax > 50 && tax >= 60)
+        {
+            taxAC = 2.29 + 1.90;
+        }
+        else if (tax > 60 && tax <= 70)
+        {
+            taxAC = 2.29 + 2.20;
+        }
+        else if (tax > 70 && tax <= 90)
+        {
+            taxAC = 2.29 + 2.50;
+        }
+        else if (tax > 90 && tax <= 100)
+        {
+            taxAC = 2.29 + 2.90;
+        }
+    }
+    else if (tax > 100 && tax <= 500)
+    {
+        taxAl = 0.10 + 10.00;
+        taxAC = 2.29 + 3.40;
+    }
+    else if (tax > 500)
+    {
+        taxAl = 0.10 + 20.00;
+        taxAC = 2.29 + 3.90;
+    }
+    return total = (tax * taxAC) + taxAl;
+    system("pause>null");
+}
--- funciones_Christian/pago.cpp
+++ funciones_Christian/pago.cpp
@@ -11,91 +11,102 @@
 ///verificar
 using namespace std;
 
-void pago(char Numero_cuenta[N][10],register_anual * registro_user, int& i)
-    {
-            char c_usuario[1];
-            int posicion = 1;
-            float total = 0;
-            float efectivo = 0;
-            char clear;
-	        bool exito = false;
-            int s = 0;
-            int vuelto = 0;
-            system("cls");
+void pago(char Numero_cuenta[N][10], register_anual *registro_user, int &i)
+{
+    char c_usuario[1];
+    int posicion = 1;
+    float total = 0;
+    float efectivo = 0;
+    char clear;
+    bool exito = false;
+    int s = 0;
+    int vuelto = 0;
+    system("cls");
 
-        do{
-            interfaz(1, 27, 1, 80);
-            interfaz(2, 26, 2, 79);
-            color(1);
-            gotoxy(27,2);cout <<"REGISTRO DE PAGO DE AGUA"<<endl;
+    do
+    {
+        interfaz(1, 27, 1, 80);
+        interfaz(2, 26, 2, 79);
+        color(1);
+        gotoxy(27, 2);
+        cout << "REGISTRO DE PAGO DE AGUA" << endl;
+        color(0);
+        gotoxy(20, 6);
+        cout << " Ingrese el numero correlativo" << endl;
+        color(0);
+        gotoxy(20, 14);
+        cout << "presione cero si desea salir " << endl;
+        gotoxy(20, 8);
+        gets(c_usuario);
+        if (verificar_registo_resibos(Numero_cuenta, c_usuario, posicion) == false)
+        {
+            color(2);
+            gotoxy(20, 18);
+            cout << "DATO INGRESADO INVALIDO" << endl;
             color(0);
-            gotoxy(20,6);cout << " Ingrese el numero correlativo" << endl;
-            color (0);
-            gotoxy(20,14);cout << "presione cero si desea salir " << endl;
-            gotoxy(20,8);gets(c_usuario);
-	if (verificar_registo_resibos(Numero_cuenta,c_usuario,posicion)==false )
-		{
-			color(2);
-			gotoxy(20,18);cout <<"DATO INGRESADO INVALIDO"<<endl;
-			color (0);
             system("pause>null");
             system("cls");
-		}
-		else
-		{
-			exito = true;
-			system("cls");
-		}
-		clear = cin.get();
-		fflush(stdin);
-    }while( (exito == false) && c_usuario[0]!= 48 );
+        }
+        else
+        {
+            exito = true;
+            system("cls");
+        }
+        clear = cin.get();
+        fflush(stdin);
+    } while ((exito == false) && c_usuario[0] != 48);
 
-
-
-        do{
-            if (exito==true)
+    do
+    {
+        if (exito == true)
+        {
+            for (int j = 1; j <= 12; j++)
             {
-                for(int j = 1; j <= 12; j++)
+                interfaz(1, 27, 1, 80);
+                interfaz(2, 26, 2, 79);
+                color(1);
+                gotoxy(27, 2);
+                cout << "REGISTRO DE PAGO DE AGUA" << endl;
+                color(1);
+                // 0 = no a pagado y 1 = ya pago
+                if ((registro_user[posicion].registro_anual[4][j] == 0) && (registro_user[posicion].registro_anual[3][j] > 0))
                 {
-                    interfaz(1, 27, 1, 80);
-                    interfaz(2, 26, 2, 79);
-                    color(1);
-                    gotoxy(27,2);cout <<"REGISTRO DE PAGO DE AGUA"<<endl;
-                    color(1);
-                    // 0 = no a pagado y 1 = ya pago
-                    if((registro_user[posicion].registro_anual[4][j] == 0) && (registro_user[posicion].registro_anual[3][j] > 0))
-                        {
-                                    gotoxy(21,5+s);cout<<"consumo de m3: "<<fixed<<setprecision(2)<<registro_user[posicion].registro_anual[2][j]<<"   costo a pagar: $"<<fixed<<setprecision(2)<<registro_user[posicion].registro_anual[3][j];
-                                    total = total + registro_user[posicion].registro_anual[3][j];
-                                    s++;
-                        }
-                }  
-                                    gotoxy(20,7+s);cout<<"Total a cancelar: $"<<total;
-                                    gotoxy(20,9+s);cout<<"Efectivo: $";
-                                    gotoxy(20,10+s);cin>>efectivo; 
-                                    system("pause>null");
-            }
-             vuelto = efectivo-total;
-            if((vuelto) < 0)
-                {
-                    s = 0;
-                    total = 0;
-                    gotoxy(31,25);cout<<"\x1b[41;37m* Su pago es menor a la deuda *\033[0m";
-                    system("pause>null"); 
-                    system("Cls");
+                    gotoxy(21, 5 + s);
+                    cout << "Consumo de m3: " << fixed << setprecision(2) << registro_user[posicion].registro_anual[2][j] << "   costo a pagar: $" << fixed << setprecision(2) << registro_user[posicion].registro_anual[3][j];
+                    total = total + registro_user[posicion].registro_anual[3][j];
+                    s++;
                 }
-                       
-        }while((vuelto) < 0);
-                gotoxy(20,9+s);cout<<"vuelto: $"<<fixed<<setprecision(2)<<efectivo-total;
-                system("pause>null");
-                for(int j = 0; j < 13; j++)
-                    {
-                        if(registro_user[posicion].registro_anual[3][j]!=0)
-                            {
-                            registro_user[posicion].registro_anual[4][j]=(1); // 1 = ya cancelo
-                            }
-                    } 
-        return;
-        system("cls");
-                
+            }
+            gotoxy(20, 7 + s);
+            cout << "Total a cancelar: $" << total;
+            gotoxy(20, 9 + s);
+            cout << "Efectivo: $";
+            gotoxy(20, 10 + s);
+            cin >> efectivo;
+            system("pause>null");
+        }
+        vuelto = efectivo - total;
+        if ((vuelto) < 0)
+        {
+            s = 0;
+            total = 0;
+            gotoxy(31, 25);
+            cout << "\x1b[41;37m* Su pago es menor a la deuda *\033[0m";
+            system("pause>null");
+            system("Cls");
+        }
+
+    } while ((vuelto) < 0);
+    gotoxy(20, 9 + s);
+    cout << "Vuelto: $" << fixed << setprecision(2) << efectivo - total;
+    system("pause>null");
+    for (int j = 0; j < 13; j++)
+    {
+        if (registro_user[posicion].registro_anual[3][j] != 0)
+        {
+            registro_user[posicion].registro_anual[4][j] = (1); // 1 = ya cancelo
+        }
     }
+    return;
+    system("cls");
+}
